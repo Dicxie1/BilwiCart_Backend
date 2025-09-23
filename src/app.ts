30ce6cb7d137009b1d2975295640afd02fd3eec7
Dicxie Madrigal <dicxiemadrigal@gmail.com>
2025-09-23 05:40:50 -0600

    feat(valid): agregar la validación de variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import healthRouter from './routers/health.router';
-import userRoutes from './routers/userRoutes'
-
-dotenv.config();
-
+import userRoutes from './routers/userRoutes';
+import { env } from './config/env';
 const app = express();
 
 app.use(express.json());
@@ -13,5 +10,5 @@ app.use('/api', healthRouter);
 app.use('/', userRoutes);
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${process.env.PORT} ${process.env.NODE_ENV}`);
+    console.log(`Servidor corriendo en http://localhost:${env.PORT} ${env.NODE_ENV}`);
 });
