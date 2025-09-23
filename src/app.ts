337a64ef17d64be95582ec9bb8360c707f34563c
Dicxie Madrigal <dicxiemadrigal@gmail.com>
2025-09-23 14:02:03 -0600

    chone: se ha refactorizado el codigo para inicializar el servidor

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import { env } from './config/env';
-import { AppDataSource } from './config/db';
+import { initializeDatasource } from './config/db';
 import { error } from 'console';
+
 const app = express();
 
 app.use(express.json());
-AppDataSource.initialize().then( () =>{
-    console.log("Conexion establecido con la base de datos!")
-    app.listen(env.PORT, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${env.PORT} ${env.NODE_ENV}`);
-});
-}).catch( (error =>{
-    console.log("Error: al conectar la base de datos " , error);
-}))
-
+initializeDatasource()
+    .then( () => {
+        app.listen(env.PORT, () =>{
+            console.log(`Servidor corriendo en http://${env.DB_HOST}: ${env.PORT}`)
+        });
+    }).catch( (error) => {
+        console.log('Error al iniciar la aplicación', error);
+        process.exit(1);
+    });
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { env } from "./env";
 
-export const AppDataSource : DataSource = new DataSource({
+export const dataSourceOptions : DataSourceOptions = {
     type : env.DB_TYPE,
     host: env.DB_HOST,
     port: env.DB_PORT,
@@ -13,4 +13,16 @@ export const AppDataSource : DataSource = new DataSource({
     entities: [__dirname + '/../models/*{.ts,.js}'],
     migrations: [],
     subscribers: [],
-});
+}
+
+export const AppDataSource : DataSource = new DataSource(dataSourceOptions);
+
+export const initializeDatasource = async () =>{
+    try{
+        AppDataSource.initialize()
+        console.log('✅ Conexión a la base de datos')
+    }catch(error){
+        console.error('❌ Error al conectar a PostgreSQL con TypeORM:', error);
+        throw error;
+    }
+}
